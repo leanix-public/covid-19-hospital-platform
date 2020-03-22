bd5391bf42367fac24292b8661c34df2b1bf6cc0
Dominik Rose <dominik.rose@leanix.net>
2020-03-22 15:07:26 +0100

    Latest version + processors

--- divi-import/divi.py
+++ divi-import/divi.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import json
 
+from random import seed
+from random import randint
+
+seed(1)
+
 with open("index.html") as fp:
     soup = BeautifulSoup(fp)
 
@@ -11,18 +16,31 @@ for tr in dataList.find_all('tr'):
     tds = tr.find_all('td')
 
     data = {}
-    data['id'] = tds[0].text.strip().split('\n')[0].strip()
+    data['id'] = tds[0].text.strip().split('\n')[0].strip().replace(";", "")
     data['type'] = 'Hospital'
     data['data'] = {
         'location': ' '.join(tds[0].text.strip().split('\n')[2:]),
         'url': tds[1].find('a')['href'] if tds[1].find('a') else None,
         'county': tds[2].text.strip() if tds[2].text.strip() != "0" else None,
-        'icuLowCare': tds[3].span.get('class')[0],
-        'icuHighCare': tds[4].span.get('class')[0],
-        'ecmo': tds[5].span.get('class')[0],
-        'university': 'Uni' in tds[0].text,
+        'icuLowCare': tds[3].span.get('class')[0].replace("hr-icon-", ""),
+        'icuLowCareId': "ICU Low Care" if tds[3].span.get('class')[0] != "hr-icon-unavailable" else None,
+        'icuHighCare': tds[4].span.get('class')[0].replace("hr-icon-", ""),
+        'icuHighCareId': "ICU High Care" if tds[4].span.get('class')[0] != "hr-icon-unavailable" else None,
+        'ecmo': tds[5].span.get('class')[0].replace("hr-icon-", ""),
+        'ecmoId': "ECMO" if tds[5].span.get('class')[0] != "hr-icon-unavailable" else None,
+        'hospitalType': 'University' if 'Uni' in tds[0].text else 'Regular',
         'updatedAt': tds[6].text.strip().split('\n')[0].strip()
     }
+
+    bedsICU = 0
+    if data['data']['icuHighCareId']: bedsICU += 100
+    if data['data']['icuLowCareId']: bedsICU += 200
+    if data['data']['ecmoId']: bedsICU += 50
+    
+    bedsICUUsed = randint(50, 80) / 100 * bedsICU
+    data['data']['bedsICU'] = bedsICU
+    data['data']['bedsICUUsed'] = bedsICUUsed
+    
     content.append(data)
 
     if (data['data']['county'] not in counties and data['data']['county']):
@@ -33,6 +51,11 @@ for county in counties:
     data['id'] = county
     data['type'] = 'County'
     content.append(data)
+
+content.append({"id": "ECMO", "type": "BC"})
+content.append({"id": "ICU Low Care", "type": "BC"})
+content.append({"id": "ICU High Care", "type": "BC"})
+
 
 ldif = {
     "connectorId": "divi",
